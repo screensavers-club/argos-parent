23cdbdaf8e5f90f6b63a9d48835857dbb1a56c83
Siah <i@siah.sg>
2021-09-03 18:29:43 +0800

    fix

--- src/argos-parent-machine.js
+++ src/argos-parent-machine.js
@@ -2,251 +2,250 @@ import { createMachine, assign, send } from "xstate";
 import axios from "axios";
 
 let argosParentMachine = createMachine(
-  {
-    id: "ArgosParent",
-    initial: "start",
-    context: {
-      identity: null,
-      room: {},
-      : [],
-      error: {},
-      rooms_available: [],
-      token: "",
-      joining_room: "",
-      master: { name: "master", vol: [0, 0.5, 1], pan: [0, 0.5, 1] },
-      input: [
-        {
-          name: "performer 1",
-          id: "p1",
-          track: "aaa",
-          vol: [0, 0.5, 1],
-          solo: false,
-          mute: false,
-          comp: false,
-          reverb: false,
-          pan: [0, 0.5, 1],
-          hpf: { in: true, freq: 0 },
-          lpf: { in: true, freq: 0 },
-          lowShelf: { in: true, freq: 0 },
-          band1: { in: true, freq: 0 },
-          band2: { in: true, freq: 0 },
-          highShelf: { in: true, freq: 0 },
-        },
-        {
-          name: "performer 2",
-          id: "p2",
-          track: "aaa",
-          vol: [0, 0.5, 1],
-          solo: false,
-          mute: false,
-          comp: false,
-          reverb: false,
-          pan: [0, 0.5, 1],
-          hpf: { in: true, freq: 0 },
-          lpf: { in: true, freq: 0 },
-          lowShelf: { in: true, freq: 0 },
-          band1: { in: true, freq: 0 },
-          band2: { in: true, freq: 0 },
-          highShelf: { in: true, freq: 0 },
-        },
-        {
-          name: "performer 3",
-          id: "p3",
-          track: "aaa",
-          vol: [0, 0.5, 1],
-          solo: false,
-          mute: false,
-          comp: false,
-          reverb: false,
-          pan: [0, 0.5, 1],
-          hpf: { in: true, freq: 0 },
-          lpf: { in: true, freq: 0 },
-          lowShelf: { in: true, freq: 0 },
-          band1: { in: true, freq: 0 },
-          band2: { in: true, freq: 0 },
-          highShelf: { in: true, freq: 0 },
-        },
-        {
-          name: "performer 4",
-          id: "p4",
-          track: "aaa",
-          vol: [0, 0.5, 1],
-          solo: false,
-          mute: false,
-          comp: false,
-          reverb: false,
-          pan: [0, 0.5, 1],
-          hpf: { in: true, freq: 0 },
-          lpf: { in: true, freq: 0 },
-          lowShelf: { in: true, freq: 0 },
-          band1: { in: true, freq: 0 },
-          band2: { in: true, freq: 0 },
-          highShelf: { in: true, freq: 0 },
-        },
-        {
-          name: "performer 5",
-          id: "p5",
-          track: "aaa",
-          vol: [0, 0.5, 1],
-          solo: false,
-          mute: false,
-          comp: false,
-          reverb: false,
-          pan: [0, 0.5, 1],
-          hpf: { in: true, freq: 0 },
-          lpf: { in: true, freq: 0 },
-          lowShelf: { in: true, freq: 0 },
-          band1: { in: true, freq: 0 },
-          band2: { in: true, freq: 0 },
-          highShelf: { in: true, freq: 0 },
-        },
-      ],
-    },
-    states: {
-      start: {
-        on: {
-          SET_IDENTITY: {
-            target: "server_connected",
-            actions: assign({
-              identity: (context, event) => {
-                return event.identity;
-              },
-            }),
-          },
-        },
-      },
+	{
+		id: "ArgosParent",
+		initial: "start",
+		context: {
+			identity: null,
+			room: {},
+			error: {},
+			rooms_available: [],
+			token: "",
+			joining_room: "",
+			master: { name: "master", vol: [0, 0.5, 1], pan: [0, 0.5, 1] },
+			input: [
+				{
+					name: "performer 1",
+					id: "p1",
+					track: "aaa",
+					vol: [0, 0.5, 1],
+					solo: false,
+					mute: false,
+					comp: false,
+					reverb: false,
+					pan: [0, 0.5, 1],
+					hpf: { in: true, freq: 0 },
+					lpf: { in: true, freq: 0 },
+					lowShelf: { in: true, freq: 0 },
+					band1: { in: true, freq: 0 },
+					band2: { in: true, freq: 0 },
+					highShelf: { in: true, freq: 0 },
+				},
+				{
+					name: "performer 2",
+					id: "p2",
+					track: "aaa",
+					vol: [0, 0.5, 1],
+					solo: false,
+					mute: false,
+					comp: false,
+					reverb: false,
+					pan: [0, 0.5, 1],
+					hpf: { in: true, freq: 0 },
+					lpf: { in: true, freq: 0 },
+					lowShelf: { in: true, freq: 0 },
+					band1: { in: true, freq: 0 },
+					band2: { in: true, freq: 0 },
+					highShelf: { in: true, freq: 0 },
+				},
+				{
+					name: "performer 3",
+					id: "p3",
+					track: "aaa",
+					vol: [0, 0.5, 1],
+					solo: false,
+					mute: false,
+					comp: false,
+					reverb: false,
+					pan: [0, 0.5, 1],
+					hpf: { in: true, freq: 0 },
+					lpf: { in: true, freq: 0 },
+					lowShelf: { in: true, freq: 0 },
+					band1: { in: true, freq: 0 },
+					band2: { in: true, freq: 0 },
+					highShelf: { in: true, freq: 0 },
+				},
+				{
+					name: "performer 4",
+					id: "p4",
+					track: "aaa",
+					vol: [0, 0.5, 1],
+					solo: false,
+					mute: false,
+					comp: false,
+					reverb: false,
+					pan: [0, 0.5, 1],
+					hpf: { in: true, freq: 0 },
+					lpf: { in: true, freq: 0 },
+					lowShelf: { in: true, freq: 0 },
+					band1: { in: true, freq: 0 },
+					band2: { in: true, freq: 0 },
+					highShelf: { in: true, freq: 0 },
+				},
+				{
+					name: "performer 5",
+					id: "p5",
+					track: "aaa",
+					vol: [0, 0.5, 1],
+					solo: false,
+					mute: false,
+					comp: false,
+					reverb: false,
+					pan: [0, 0.5, 1],
+					hpf: { in: true, freq: 0 },
+					lpf: { in: true, freq: 0 },
+					lowShelf: { in: true, freq: 0 },
+					band1: { in: true, freq: 0 },
+					band2: { in: true, freq: 0 },
+					highShelf: { in: true, freq: 0 },
+				},
+			],
+		},
+		states: {
+			start: {
+				on: {
+					SET_IDENTITY: {
+						target: "server_connected",
+						actions: assign({
+							identity: (context, event) => {
+								return event.identity;
+							},
+						}),
+					},
+				},
+			},
 
-      error: {
-        context: {
-          room: "error",
-          error: (context, event) => {
-            return { ...context.error, message: event.data.message };
-          },
-        },
-      },
+			error: {
+				context: {
+					room: "error",
+					error: (context, event) => {
+						return { ...context.error, message: event.data.message };
+					},
+				},
+			},
 
-      server_connected: {
-        on: {
-          CREATE_ROOM: { target: "create_room" },
-          JOIN_ROOM: { target: "select_room" },
-        },
-      },
+			server_connected: {
+				on: {
+					CREATE_ROOM: { target: "create_room" },
+					JOIN_ROOM: { target: "select_room" },
+				},
+			},
 
-      create_room: {
-        invoke: {
-          id: "fetch_room_name",
-          src: (context, event) => {
-            return axios.get(
-              `${process.env.REACT_APP_PEER_SERVER}/generate-room-name`
-            );
-          },
+			create_room: {
+				invoke: {
+					id: "fetch_room_name",
+					src: (context, event) => {
+						return axios.get(
+							`${process.env.REACT_APP_PEER_SERVER}/generate-room-name`
+						);
+					},
 
-          onDone: {
-            actions: assign({
-              room: (context, event) => {
-                return { ...context.room, name: event.data.data.name };
-              },
-            }),
-          },
+					onDone: {
+						actions: assign({
+							room: (context, event) => {
+								return { ...context.room, name: event.data.data.name };
+							},
+						}),
+					},
 
-          onError: {
-            target: "error",
-            actions: assign({
-              error: (context, event) => {
-                return { message: event.data.message };
-              },
-            }),
-          },
-        },
+					onError: {
+						target: "error",
+						actions: assign({
+							error: (context, event) => {
+								return { message: event.data.message };
+							},
+						}),
+					},
+				},
 
-        on: {
-          RECEIVE_TOKEN: {
-            target: "stream_room",
-            actions: assign({
-              token: (context, event) => {
-                return event.token;
-              },
-            }),
-          },
+				on: {
+					RECEIVE_TOKEN: {
+						target: "stream_room",
+						actions: assign({
+							token: (context, event) => {
+								return event.token;
+							},
+						}),
+					},
 
-          SET_NEW_ROOM_NAME: {
-            actions: assign({
-              room: (context, event) => {
-                return { ...context.room, name: event.name };
-              },
-            }),
-          },
-        },
-      },
+					SET_NEW_ROOM_NAME: {
+						actions: assign({
+							room: (context, event) => {
+								return { ...context.room, name: event.name };
+							},
+						}),
+					},
+				},
+			},
 
-      stream_room: {},
+			stream_room: {},
 
-      select_room: {
-        on: {
-          ROOM_SELECTED: {
-            target: "enter_password",
-            actions: assign({
-              joining_room: (context, event) => {
-                console.log(event.room);
-                return event.room;
-              },
-            }),
-          },
+			select_room: {
+				on: {
+					ROOM_SELECTED: {
+						target: "enter_password",
+						actions: assign({
+							joining_room: (context, event) => {
+								console.log(event.room);
+								return event.room;
+							},
+						}),
+					},
 
-          RETRIEVE_ROOMS: {
-            actions: assign({
-              rooms_available: (context, event) => {
-                // console.log(event.rooms_available);
-                return event.rooms_available;
-              },
-            }),
-          },
-        },
-      },
+					RETRIEVE_ROOMS: {
+						actions: assign({
+							rooms_available: (context, event) => {
+								// console.log(event.rooms_available);
+								return event.rooms_available;
+							},
+						}),
+					},
+				},
+			},
 
-      enter_password: {
-        on: {
-          CHECK_INPUT: {
-            target: "stream_room",
-            actions: assign({
-              token: (context, event) => {
-                return event.token;
-              },
-              room: (context, event) => {
-                return { name: context.joining_room };
-              },
-            }),
-          },
-        },
-      },
+			enter_password: {
+				on: {
+					CHECK_INPUT: {
+						target: "stream_room",
+						actions: assign({
+							token: (context, event) => {
+								return event.token;
+							},
+							room: (context, event) => {
+								return { name: context.joining_room };
+							},
+						}),
+					},
+				},
+			},
 
-      room_joined: {
-        context: {
-          room: "room_joined",
-          id: "room_joined",
-        },
-      },
-    },
+			room_joined: {
+				context: {
+					room: "room_joined",
+					id: "room_joined",
+				},
+			},
+		},
 
-    on: {
-      actions: assign({
-        room: (context, event) => {
-          return { ...context.room, name: event.data.room };
-        },
-      }),
-      RESET: { target: "server_connected", room: "Not Connected" },
-    },
-  },
-  {
-    actions: {
-      assign_room_name: (context, event) => {
-        console.log(context, event);
-        if (event.type === "SUCCESS") {
-          context.room.name = event.name;
-        }
-      },
-    },
-  }
+		on: {
+			actions: assign({
+				room: (context, event) => {
+					return { ...context.room, name: event.data.room };
+				},
+			}),
+			RESET: { target: "server_connected", room: "Not Connected" },
+		},
+	},
+	{
+		actions: {
+			assign_room_name: (context, event) => {
+				console.log(context, event);
+				if (event.type === "SUCCESS") {
+					context.room.name = event.name;
+				}
+			},
+		},
+	}
 );
 
 export default argosParentMachine;
