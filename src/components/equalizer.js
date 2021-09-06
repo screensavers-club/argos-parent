8491a00b9fbb8b84c45deb06f815f770f74a15cb
bananaleow <bryanleowek@gmail.com>
2021-09-06 14:42:05 +0800

    feat: frequency controls

--- src/components/equalizer.js
+++ src/components/equalizer.js
@@ -78,6 +78,7 @@ export default function Equalizer({ toggleEQ, control, setControl }) {
                 <div className="inputDiv">
                   <span>frequency</span>
                   <input
+                    defaultValue={0}
                     style={{ width: "100%" }}
                     value={control[toggleEQ].eq[i].freq}
                     onChange={(value) => {
@@ -88,7 +89,49 @@ export default function Equalizer({ toggleEQ, control, setControl }) {
                     }}
                   />
                 </div>
-                <div className="inputRange"></div>
+                <Range
+                  trackStyle={[
+                    { backgroundColor: "black" },
+                    { backgroundColor: "transparent" },
+                    { backgroundColor: "black" },
+                  ]}
+                  handleStyle={[
+                    {
+                      backgroundColor: "transparent",
+                      borderColor: "transparent",
+                    },
+                    {
+                      backgroundColor: "transparent",
+                      borderColor: "transparent",
+                    },
+                    {
+                      backgroundColor: "white",
+                      borderRadius: "25px",
+                      width: "1.5em",
+                      left: "0",
+                      border: "1px solid black",
+                    },
+                  ]}
+                  railStyle={{ border: "1px solid black" }}
+                  min={0}
+                  max={1}
+                  step={0.05}
+                  style={{
+                    margin: "10px auto",
+                    padding: "auto",
+                    width: "100px",
+                  }}
+                  value={[0, control[toggleEQ].eq[i].freq]}
+                  onChange={(value) => {
+                    let _control = control.slice(0);
+                    _control[toggleEQ].eq[i].freq = value[1];
+                    setControl(_control);
+                    if (value[0] > 0) {
+                      value[0] = 0;
+                    }
+                    console.log(control);
+                  }}
+                />
               </div>
             </div>
           );
