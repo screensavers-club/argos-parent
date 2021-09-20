e33da2d84f8677bed40148b8ee63ee6cc3a1007e
Siah <i@siah.sg>
2021-09-20 13:45:31 +0800

    fix: indent

--- src/pages/stream-room.js
+++ src/pages/stream-room.js
@@ -9,405 +9,405 @@ import VideoLayouts from "../util/video-layouts";
 import { DataPacket_Kind, RoomEvent } from "livekit-client";
 
 const StyledPage = styled.div`
-position: relative;
-display: block;
-div.roomCreated {
-text-align: center;
-font-size: 1.5rem;
-margin-top: 10%;
-}
-table.participants {
-border: 1px solid black;
-padding: 25px;
-caption {
-border: 1px solid black;
-border-bottom: 2px solid black;
-font-size: 18px;
-font-weight: normal;
-}
-tr.id {
-min-width: 200px;
-}
-thead {
-> div {
-border-top: 2px solid black;
-padding-top: 10px;
-margin-bottom: 10px;
-}
-}
-tbody {
-> div {
-padding-top: 10px;
-margin-bottom: 10px;
-border-top: 2px solid black;
-}
-}
-}
-div.stream {
-position: relative;
-display: flex;
-justify-content: center;
-border: 1px solid black;
-padding: 10px;
-margin: 25px;
-div.userVideos {
-position: relative;
-border: 1px solid red;
-min-width: 50%;
-display: flex;
-}
-}
-div.button {
-position: absolute;
-left: 25px;
-top: 0;
-margin: auto;
-padding: auto;
-}
-div.controlPanel {
-display: block;
-margin: auto;
-padding: auto;
-}
-div.exitingModal {
-display: none;
-}
-div.active {
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-border: 1px solid black;
-background: white;
-position: fixed;
-width: 30%;
-height: 30%;
-left: 50%;
-top: 50%;
-transform: translate(-50%, -50%);
-div {
-width: 100%;
-margin-top: 25px;
-display: inline-flex;
-justify-content: center;
-> button {
-padding: 5px;
-display: flex;
-justify-content: center;
-align-content: center;
-margin: 5px;
-min-width: 50px;
-~ .no {
-background: #f25555;
-color: white;
-:hover {
-cursor: pointer;
-background: #f22222;
-}
-}
-}
-}
-}
+	position: relative;
+	display: block;
+	div.roomCreated {
+		text-align: center;
+		font-size: 1.5rem;
+		margin-top: 10%;
+	}
+	table.participants {
+		border: 1px solid black;
+		padding: 25px;
+		caption {
+			border: 1px solid black;
+			border-bottom: 2px solid black;
+			font-size: 18px;
+			font-weight: normal;
+		}
+		tr.id {
+			min-width: 200px;
+		}
+		thead {
+			> div {
+				border-top: 2px solid black;
+				padding-top: 10px;
+				margin-bottom: 10px;
+			}
+		}
+		tbody {
+			> div {
+				padding-top: 10px;
+				margin-bottom: 10px;
+				border-top: 2px solid black;
+			}
+		}
+	}
+	div.stream {
+		position: relative;
+		display: flex;
+		justify-content: center;
+		border: 1px solid black;
+		padding: 10px;
+		margin: 25px;
+		div.userVideos {
+			position: relative;
+			border: 1px solid red;
+			min-width: 50%;
+			display: flex;
+		}
+	}
+	div.button {
+		position: absolute;
+		left: 25px;
+		top: 0;
+		margin: auto;
+		padding: auto;
+	}
+	div.controlPanel {
+		display: block;
+		margin: auto;
+		padding: auto;
+	}
+	div.exitingModal {
+		display: none;
+	}
+	div.active {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		border: 1px solid black;
+		background: white;
+		position: fixed;
+		width: 30%;
+		height: 30%;
+		left: 50%;
+		top: 50%;
+		transform: translate(-50%, -50%);
+		div {
+			width: 100%;
+			margin-top: 25px;
+			display: inline-flex;
+			justify-content: center;
+			> button {
+				padding: 5px;
+				display: flex;
+				justify-content: center;
+				align-content: center;
+				margin: 5px;
+				min-width: 50px;
+				~ .no {
+					background: #f25555;
+					color: white;
+					:hover {
+						cursor: pointer;
+						background: #f22222;
+					}
+				}
+			}
+		}
+	}
 `;
 export default function StreamRoom({ context, send, parents }) {
-const { room, connect, participants, audioTracks } = useRoom();
-const [selectTab, setSelectTab] = useState("stream");
-const [renderState, setRenderState] = useState(0);
-let [exiting, setExiting] = useState(false);
-const exitingModalRef = useRef();
-const audioCtx = useRef(new AudioContext());
-const msDestination = useRef(null);
-const audioTrackRefs = useRef({});
-const [audioTrackRefsState, setAudioTrackRefsState] = useState(
-audioTrackRefs.current
-);
-const videoTrackRefs = useRef({});
-const [videoTrackRefsState, setVideoTrackRefsState] = useState(
-videoTrackRefs.current
-);
-useEffect(() => {
-//create mediaStreamDestination
-msDestination.current = audioCtx.current.createMediaStreamDestination();
-  
-  document.addEventListener("mousedown", handleClick);
-document.addEventListener("keyup", handleEsc);
-return () => {
-document.removeEventListener("mousedown", handleClick);
-document.removeEventListener("keyup", handleEsc);
-};
-}, []);
-const handleClick = (e) => {
-if (exitingModalRef.current.contains(e.target)) {
-return;
-}
-setExiting(false);
-};
-const handleEsc = (e) => {
-if (e.key === "Escape") {
-setExiting(false);
-} else return;
-};
-useEffect(() => {
-let __tracks = [];
-audioTracks.forEach((audioTrack) => {
-if (__tracks.indexOf(audioTrack.sid) < 0 && audioTrack.mediaStreamTrack) {
-__tracks.push(audioTrack.sid);
-let mst = audioCtx.current.createMediaStreamTrackSource(
-audioTrack.mediaStreamTrack
-);
-let gainNode = new GainNode(audioCtx.current, { gain: 1 });
-let channelSplitterNode = new ChannelSplitterNode(audioCtx.current, {
-numberOfOutputs: 2,
-});
-audioTrackRefs.current[audioTrack.sid] = {
-mediaStreamTrackSource: mst,
-gainNode: gainNode,
-splitter: channelSplitterNode,
-};
-mst.connect(channelSplitterNode).connect(audioCtx.current.destination);
-channelSplitterNode.connect(gainNode).connect(msDestination.current);
-}
-});
-Object.keys(audioTrackRefs.current).forEach((key) => {
-if (__tracks.indexOf(key) < 0) {
-audioTrackRefs.current[key].mediaStreamTrackSource.disconnect();
-audioTrackRefs.current[key].gainNode.disconnect();
-audioTrackRefs.current[key].splitter.disconnect();
-delete audioTrackRefs.current[key];
-}
-});
-setAudioTrackRefsState(audioTrackRefs.current);
-setRenderState(renderState + 1);
-}, [audioTracks]);
-useEffect(() => {
-let __tracks = [];
-participants.forEach((participant) => {
-if (participant.videoTracks.size < 1) {
-return;
-}
-let firstVideo = null;
-participant.videoTracks.forEach((track, key) => {
-if (!firstVideo) {
-firstVideo = { track, key };
-} else {
-return;
-}
-});
-if (__tracks.indexOf(firstVideo.key) < 0) {
-__tracks.push(firstVideo.key);
-videoTrackRefs.current[firstVideo.key] = {
-videoTrack: firstVideo.track,
-identity: participant.identity,
-};
-}
-});
-Object.keys(videoTrackRefs.current).forEach((key) => {
-if (__tracks.indexOf(key) < 0) {
-delete videoTrackRefs.current[key];
-}
-});
-setVideoTrackRefsState(videoTrackRefs.current);
-setRenderState(renderState + 1);
-}, [participants]);
-const [control, setControl] = useState(context.input);
-useEffect(() => {
-connect(`${process.env.REACT_APP_LIVEKIT_SERVER}`, context.token)
-.then((room) => {
-console.log("room connected");
-})
-.catch((err) => console.log({ err }));
-return () => {
-room?.disconnect();
-};
-}, []);
-return (
-<StyledPage>
-<div className="button">
-<Button
-onClick={() => {
-setExiting(true);
-}}
-variant="small"
->
-Disconnect
-</Button>
-</div>
-<div
-className={`exitingModal ${exiting === true ? "active" : ""}`}
-ref={exitingModalRef}
->
-Are you sure you want to exiting?
-<div>
-<Button
-variant="small"
-className="yes"
-onClick={() => {
-room?.disconnect();
-send("DISCONNECT");
-setExiting(false);
-}}
->
-yes
-</Button>
-<Button
-variant="small"
-className="no"
-onClick={() => {
-setExiting(false);
-}}
->
-no
-</Button>
-</div>
-</div>
-<StreamTabs setSelectTab={setSelectTab} />
-{(function () {
-switch (selectTab) {
-case "stream":
-return (
-<>
-<MainControlView>
-<div className="participants">
-{participants
-.filter((p) => p.metadata === "CHILD")
-.map((participant) => {
-let firstAudioTrack = null;
-participant.audioTracks.forEach((value, key) => {
-if (!firstAudioTrack) {
-firstAudioTrack = value;
-}
-});
-let trackRef =
-audioTrackRefsState[firstAudioTrack?.trackSid];
-return (
-<div
-style={{ borderBottom: "1px solid #aaa" }}
-key={participant.identity}
->
-<span className="user">{participant.identity}</span>
-{trackRef && (
-<button
-onClick={() => {
-if (trackRef && trackRef?.gainNode) {
-let targetGain =
-trackRef.gainNode.gain.value === 0
-? 1
-: 0;
-trackRef.gainNode.gain.setValueAtTime(
-targetGain,
-audioCtx.current.currentTime
-);
-}
-setRenderState(renderState + 1);
-}}
->
-{trackRef?.gainNode?.gain?.value
-? "Mute"
-: "Unmute"}
-</button>
-)}
-</div>
-);
-})}
-</div>
-<div className="videos">
-{Object.keys(videoTrackRefsState).map((key, i) => {
-if (!videoTrackRefsState[key]?.videoTrack?.track) {
-return <></>;
-}
-return (
-<div key={key}>
-<VideoFrame
-track={videoTrackRefsState[key]}
-key={key}
-/>
-<div style={{ display: "flex" }}>
-<input
-type="text"
-id={`stream_url_${key}`}
-value={`${process.env.REACT_APP_VIEWER_BASE_URL}?room=${context.room.name}&passcode=${context.passcode}&target=${videoTrackRefsState[key].identity}`}
-/>
-<button
-type="button"
-onClick={() => {
-let input = document.querySelector(
-`#${`stream_url_${key}`}`
-);
-input.select();
-document.execCommand("copy");
-}}
->
-Copy
-</button>
-</div>
-</div>
-);
-})}
-</div>
-<div>Controls</div>
-</MainControlView>
-</>
-);
-case "layout":
-return (
-<VideoLayoutEditor
-room={room}
-participants={participants}
-videoTrackRefsState={videoTrackRefsState}
-onChange={() => {}}
-/>
-);
-case "mixer":
-return (
-<div>
-<div style={{ display: "flex" }}>
-<input
-type="text"
-id={`stream_url_${context.identity}`}
-value={`${process.env.REACT_APP_VIEWER_BASE_URL}?room=${context.room.name}&passcode=${context.passcode}&target=${context.identity}`}
-/>
-<button
-type="button"
-onClick={() => {
-// let input = document.querySelector(
-//   `#${`stream_url_${key}`}`
-// );
-// input.select();
-// document.execCommand("copy");
-}}
->
-Copy
-</button>
-<button
-type="button"
-onClick={() => {
-let track = msDestination.current?.stream?.getTracks();
-if (track[0]) {
-console.log("publish track");
-room.localParticipant.publishTrack(track[0]);
-}
-}}
->
-Test
-</button>
-</div>
-<MixerPage
-control={control}
-setControl={setControl}
-master={context.master}
-/>
-</div>
-);
-case "monitor":
-return <>This is the monitor page</>;
-case "out":
-return <>This is the out page</>;
-default:
-<></>;
-}
-})()}
-</StyledPage>
-);
+	const { room, connect, participants, audioTracks } = useRoom();
+	const [selectTab, setSelectTab] = useState("stream");
+	const [renderState, setRenderState] = useState(0);
+	let [exiting, setExiting] = useState(false);
+	const exitingModalRef = useRef();
+	const audioCtx = useRef(new AudioContext());
+	const msDestination = useRef(null);
+	const audioTrackRefs = useRef({});
+	const [audioTrackRefsState, setAudioTrackRefsState] = useState(
+		audioTrackRefs.current
+	);
+	const videoTrackRefs = useRef({});
+	const [videoTrackRefsState, setVideoTrackRefsState] = useState(
+		videoTrackRefs.current
+	);
+	useEffect(() => {
+		//create mediaStreamDestination
+		msDestination.current = audioCtx.current.createMediaStreamDestination();
+
+		document.addEventListener("mousedown", handleClick);
+		document.addEventListener("keyup", handleEsc);
+		return () => {
+			document.removeEventListener("mousedown", handleClick);
+			document.removeEventListener("keyup", handleEsc);
+		};
+	}, []);
+	const handleClick = (e) => {
+		if (exitingModalRef.current.contains(e.target)) {
+			return;
+		}
+		setExiting(false);
+	};
+	const handleEsc = (e) => {
+		if (e.key === "Escape") {
+			setExiting(false);
+		} else return;
+	};
+	useEffect(() => {
+		let __tracks = [];
+		audioTracks.forEach((audioTrack) => {
+			if (__tracks.indexOf(audioTrack.sid) < 0 && audioTrack.mediaStreamTrack) {
+				__tracks.push(audioTrack.sid);
+				let mst = audioCtx.current.createMediaStreamTrackSource(
+					audioTrack.mediaStreamTrack
+				);
+				let gainNode = new GainNode(audioCtx.current, { gain: 1 });
+				let channelSplitterNode = new ChannelSplitterNode(audioCtx.current, {
+					numberOfOutputs: 2,
+				});
+				audioTrackRefs.current[audioTrack.sid] = {
+					mediaStreamTrackSource: mst,
+					gainNode: gainNode,
+					splitter: channelSplitterNode,
+				};
+				mst.connect(channelSplitterNode).connect(audioCtx.current.destination);
+				channelSplitterNode.connect(gainNode).connect(msDestination.current);
+			}
+		});
+		Object.keys(audioTrackRefs.current).forEach((key) => {
+			if (__tracks.indexOf(key) < 0) {
+				audioTrackRefs.current[key].mediaStreamTrackSource.disconnect();
+				audioTrackRefs.current[key].gainNode.disconnect();
+				audioTrackRefs.current[key].splitter.disconnect();
+				delete audioTrackRefs.current[key];
+			}
+		});
+		setAudioTrackRefsState(audioTrackRefs.current);
+		setRenderState(renderState + 1);
+	}, [audioTracks]);
+	useEffect(() => {
+		let __tracks = [];
+		participants.forEach((participant) => {
+			if (participant.videoTracks.size < 1) {
+				return;
+			}
+			let firstVideo = null;
+			participant.videoTracks.forEach((track, key) => {
+				if (!firstVideo) {
+					firstVideo = { track, key };
+				} else {
+					return;
+				}
+			});
+			if (__tracks.indexOf(firstVideo.key) < 0) {
+				__tracks.push(firstVideo.key);
+				videoTrackRefs.current[firstVideo.key] = {
+					videoTrack: firstVideo.track,
+					identity: participant.identity,
+				};
+			}
+		});
+		Object.keys(videoTrackRefs.current).forEach((key) => {
+			if (__tracks.indexOf(key) < 0) {
+				delete videoTrackRefs.current[key];
+			}
+		});
+		setVideoTrackRefsState(videoTrackRefs.current);
+		setRenderState(renderState + 1);
+	}, [participants]);
+	const [control, setControl] = useState(context.input);
+	useEffect(() => {
+		connect(`${process.env.REACT_APP_LIVEKIT_SERVER}`, context.token)
+			.then((room) => {
+				console.log("room connected");
+			})
+			.catch((err) => console.log({ err }));
+		return () => {
+			room?.disconnect();
+		};
+	}, []);
+	return (
+		<StyledPage>
+			<div className="button">
+				<Button
+					onClick={() => {
+						setExiting(true);
+					}}
+					variant="small"
+				>
+					Disconnect
+				</Button>
+			</div>
+			<div
+				className={`exitingModal ${exiting === true ? "active" : ""}`}
+				ref={exitingModalRef}
+			>
+				Are you sure you want to exiting?
+				<div>
+					<Button
+						variant="small"
+						className="yes"
+						onClick={() => {
+							room?.disconnect();
+							send("DISCONNECT");
+							setExiting(false);
+						}}
+					>
+						yes
+					</Button>
+					<Button
+						variant="small"
+						className="no"
+						onClick={() => {
+							setExiting(false);
+						}}
+					>
+						no
+					</Button>
+				</div>
+			</div>
+			<StreamTabs setSelectTab={setSelectTab} />
+			{(function () {
+				switch (selectTab) {
+					case "stream":
+						return (
+							<>
+								<MainControlView>
+									<div className="participants">
+										{participants
+											.filter((p) => p.metadata === "CHILD")
+											.map((participant) => {
+												let firstAudioTrack = null;
+												participant.audioTracks.forEach((value, key) => {
+													if (!firstAudioTrack) {
+														firstAudioTrack = value;
+													}
+												});
+												let trackRef =
+													audioTrackRefsState[firstAudioTrack?.trackSid];
+												return (
+													<div
+														style={{ borderBottom: "1px solid #aaa" }}
+														key={participant.identity}
+													>
+														<span className="user">{participant.identity}</span>
+														{trackRef && (
+															<button
+																onClick={() => {
+																	if (trackRef && trackRef?.gainNode) {
+																		let targetGain =
+																			trackRef.gainNode.gain.value === 0
+																				? 1
+																				: 0;
+																		trackRef.gainNode.gain.setValueAtTime(
+																			targetGain,
+																			audioCtx.current.currentTime
+																		);
+																	}
+																	setRenderState(renderState + 1);
+																}}
+															>
+																{trackRef?.gainNode?.gain?.value
+																	? "Mute"
+																	: "Unmute"}
+															</button>
+														)}
+													</div>
+												);
+											})}
+									</div>
+									<div className="videos">
+										{Object.keys(videoTrackRefsState).map((key, i) => {
+											if (!videoTrackRefsState[key]?.videoTrack?.track) {
+												return <></>;
+											}
+											return (
+												<div key={key}>
+													<VideoFrame
+														track={videoTrackRefsState[key]}
+														key={key}
+													/>
+													<div style={{ display: "flex" }}>
+														<input
+															type="text"
+															id={`stream_url_${key}`}
+															value={`${process.env.REACT_APP_VIEWER_BASE_URL}?room=${context.room.name}&passcode=${context.passcode}&target=${videoTrackRefsState[key].identity}`}
+														/>
+														<button
+															type="button"
+															onClick={() => {
+																let input = document.querySelector(
+																	`#${`stream_url_${key}`}`
+																);
+																input.select();
+																document.execCommand("copy");
+															}}
+														>
+															Copy
+														</button>
+													</div>
+												</div>
+											);
+										})}
+									</div>
+									<div>Controls</div>
+								</MainControlView>
+							</>
+						);
+					case "layout":
+						return (
+							<VideoLayoutEditor
+								room={room}
+								participants={participants}
+								videoTrackRefsState={videoTrackRefsState}
+								onChange={() => {}}
+							/>
+						);
+					case "mixer":
+						return (
+							<div>
+								<div style={{ display: "flex" }}>
+									<input
+										type="text"
+										id={`stream_url_${context.identity}`}
+										value={`${process.env.REACT_APP_VIEWER_BASE_URL}?room=${context.room.name}&passcode=${context.passcode}&target=${context.identity}`}
+									/>
+									<button
+										type="button"
+										onClick={() => {
+											// let input = document.querySelector(
+											//   `#${`stream_url_${key}`}`
+											// );
+											// input.select();
+											// document.execCommand("copy");
+										}}
+									>
+										Copy
+									</button>
+									<button
+										type="button"
+										onClick={() => {
+											let track = msDestination.current?.stream?.getTracks();
+											if (track[0]) {
+												console.log("publish track");
+												room.localParticipant.publishTrack(track[0]);
+											}
+										}}
+									>
+										Test
+									</button>
+								</div>
+								<MixerPage
+									control={control}
+									setControl={setControl}
+									master={context.master}
+								/>
+							</div>
+						);
+					case "monitor":
+						return <>This is the monitor page</>;
+					case "out":
+						return <>This is the out page</>;
+					default:
+						<></>;
+				}
+			})()}
+		</StyledPage>
+	);
 }
 
 function VideoFrame({ track }) {
