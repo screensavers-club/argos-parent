4b3d124f211ddecd274a4a30b72b9d3e811e4e7e
Siah Tiong Hong <siah@goodwork.sg>
2021-10-05 14:59:15 +0800

    feat: browser support notice

--- src/App.js
+++ src/App.js
@@ -20,60 +20,80 @@ import { inspect } from "@xstate/inspect";
 // inspect({ iframe: false });
 
 function App() {
-	let [state, send] = useMachine(argosParentMachine, {
-		devTools:
-			process.env.NODE_ENV === "development" && typeof window !== "undefined",
-	});
+  let [state, send] = useMachine(argosParentMachine, {
+    devTools:
+      process.env.NODE_ENV === "development" && typeof window !== "undefined",
+  });
 
-	return (
-		<div className="App">
-			<AppFrame>
-				<StatusBar room={_.get(state, "context.room.name")} version="0.2.0" />
-				<Screen state={state.value} context={state.context} send={send} />
-			</AppFrame>
-		</div>
-	);
+  let [supported, setSupported] = useState(
+    (() => {
+      let _ac = new AudioContext();
+      let msts = _ac.createMediaStreamTrackSource;
+      _ac = null;
+      return typeof msts === "function";
+    })()
+  );
+
+  return (
+    <div className="App">
+      <AppFrame>
+        <StatusBar room={_.get(state, "context.room.name")} version="0.2.0" />
+
+        {supported ? (
+          <Screen state={state.value} context={state.context} send={send} />
+        ) : (
+          <div style={{ textAlign: "center", padding: "2em" }}>
+            <h1>Browser not supported.</h1>
+            <p>
+              Please access this application using the latest version of
+              Firefox.
+            </p>
+          </div>
+        )}
+      </AppFrame>
+    </div>
+  );
 }
 
 export default App;
 
 function Screen({ context, state, send }) {
-	switch (state) {
-		case "start":
-			return <StartScreen send={send} />;
+  switch (state) {
+    case "start":
+      return <StartScreen send={send} />;
 
-		case "server_connected":
-			return <RoomStartScreen send={send} />;
+    case "server_connected":
+      return <RoomStartScreen send={send} />;
 
-		case "error":
-			return <ErrorScreen send={send} context={context} />;
+    case "error":
+      return <ErrorScreen send={send} context={context} />;
 
-		case "create_room":
-			return <FetchedRoom send={send} context={context} />;
+    case "create_room":
+      return <FetchedRoom send={send} context={context} />;
 
-		case "stream_room":
-			return (
-				<StreamRoom
-					send={send}
-					context={context}
-					error={_.get(state, "context.error.message")}
-					parents={[
-						{ id: "Parent 1", status: "host" },
-						{ id: "Parent 2", status: "backup" },
-					]}
-				/>
-			);
+    case "stream_room":
+      return (
+        <StreamRoom
+          send={send}
+          context={context}
+          error={_.get(state, "context.error.message")}
+          parents={[
+            { id: "Parent 1", status: "host" },
+            { id: "Parent 2", status: "backup" },
+          ]}
+        />
+      );
 
-		case "select_room":
-			return <SelectRoom send={send} context={context} />;
+    case "select_room":
+      return <SelectRoom send={send} context={context} />;
 
-		case "enter_password":
-			return <EnterPassword send={send} context={context} />;
+    case "enter_password":
+      return <EnterPassword send={send} context={context} />;
 
-		case "room_joined":
-			return <RoomJoined send={send} context={context} />;
+    case "room_joined":
+      return <RoomJoined send={send} context={context} />;
 
-		default:
-			return <></>;
-	}
+    default:
+      return <></>;
+  }
 }
